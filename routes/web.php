01b18a005b8703ce2d5d1991b8ba06b1daea0b15
David wanjohi <davidwagura83@gmail.com>
2024-03-01 13:06:48 +0300

    add route which prevents access to guest and check if user have verified their email

--- routes/web.php
+++ routes/web.php
@@ -27,5 +27,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+Route::get('/only-verified', function () {
+    return view('only-verified');
+})->middleware(['auth', 'verified']);
 
 require __DIR__.'/auth.php';
